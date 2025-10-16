07bee7b2341a698bb7c91a37639c3b5217064ee3
PoojithaKS <138577436+PoojithaKS@users.noreply.github.com>
2025-10-16 13:31:25 +0530

    Update header to 'Add & Delete tasks'

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $tasks = $db->query('SELECT * FROM tasks');
 <html>
 <head><title>Task Manager</title><link rel='stylesheet' href='style.css'></head>
 <body>
-<h1>Task Manager</h1>
+<h1>Add & Delete tasks</h1>
 <form action='add.php' method='POST'>
 <input type='text' name='task' required>
 <button type='submit'>Add Task</button>
